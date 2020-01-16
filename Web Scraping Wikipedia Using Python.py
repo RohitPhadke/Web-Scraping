5a7e87388aa66527d3981d5c3539c4f4b8ddb7ce
Rohit Phadke <37411375+RohitPhadke@users.noreply.github.com>
2020-01-16 18:05:12 -0500

    Add files via upload

---
+++ Web Scraping Wikipedia Using Python.py
@@ -0,0 +1,32 @@
+#Step 1: Import libraries for Web Scrapping
+import nltk
+import urllib
+import bs4 as bs
+import re
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+nltk.download('punkt')
+#Getting the data source
+source = urllib.request.urlopen('https://en.wikipedia.org/wiki/Global_warming').read()
+
+#Parsing the data / creating Beautiful Soup object
+soup = bs.BeautifulSoup(source,'lxml')
+
+#Fetching the data
+text = ""
+for paragraph in soup.find_all('p'):
+    text += paragraph.text
+    
+#Preprocessing the data
+text = re.sub(r'\[[0-9]*\]',' ',text)
+text = re.sub(r'\s+',' ',text)
+text = text.lower()
+text = re.sub(r'\d',' ',text)
+text = re.sub(r'\s+',' ',text)
+text = re.sub(r'\s+',' ',text)
+text = re.sub(r'\s+',' ',text)
+
+#Preparing the dataset
+sentences = nltk.sent_tokenize(text)
+
+sentences = [nltk.word_tokenize(sentence) for sentence in sentences]
